b04f4da405ff08bb543691af39d5f92f6c0579e2
Tim Schindler <tim@catalyst-zero.com>
2014-06-10 08:35:30 +0000

    initial commit

--- api_schema.go
+++ api_schema.go
@@ -12,27 +12,6 @@ type ResponsePayload struct {
 	Data       interface{} `json:"data,omitempty"`
 }
 
-func IsStatus(statusCode int, responseBody string) (bool, error) {
-  var responsePayload ResponsePayload
-  if err := json.Unmarshal([]byte(responseBody), &responsePayload); err != nil {
-    return false, errgo.Mask(err)
-  }
-
-  if responsePayload.StatusCode == STATUS_CODE_DATA {
-    return true, nil
-  }
-
-  return false, nil
-}
-
-func IsSuccessResponse(statusCode int) bool {
-  return statusCode == http.StatusOK
-}
-
-func IsFailureResponse(statusCode int) bool {
-  return statusCode == http.StatusInternalServerError
-}
-
 // Responder method.
 func StatusData(data interface{}) ResponsePayload {
 	return ResponsePayload{
---
+++ util.go
@@ -0,0 +1,23 @@
+package apischema
+
+func IsStatus(statusCode int, responseBody string) (bool, error) {
+  var responsePayload ResponsePayload
+  if err := json.Unmarshal([]byte(responseBody), &responsePayload); err != nil {
+    return false, errgo.Mask(err)
+  }
+
+  if responsePayload.StatusCode == STATUS_CODE_DATA {
+    return true, nil
+  }
+
+  return false, nil
+}
+
+func IsSuccessResponse(statusCode int) bool {
+  return statusCode == http.StatusOK
+}
+
+func IsFailureResponse(statusCode int) bool {
+  return statusCode == http.StatusInternalServerError
+}
+

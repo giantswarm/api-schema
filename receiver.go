4b5b02f125b21ef96348a6fd7efb7c89bf9ccb4b
Tim Schindler <tim@catalyst-zero.com>
2014-07-31 22:16:39 +0000

    fixed reading response body twice

--- receiver.go
+++ receiver.go
@@ -4,34 +4,34 @@ import (
 	"io"
 )
 
-func IsStatusData(responseBody io.ReadCloser) (bool, error) {
+func IsStatusData(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_DATA, responseBody)
 }
 
-func IsStatusRessourceCreated(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceCreated(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_CREATED, responseBody)
 }
 
-func IsStatusRessourceStarted(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceStarted(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_STARTED, responseBody)
 }
 
-func IsStatusRessourceStopped(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceStopped(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_STOPPED, responseBody)
 }
 
-func IsStatusRessourceUpdated(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceUpdated(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_UPDATED, responseBody)
 }
 
-func IsStatusRessourceDeleted(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceDeleted(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_DELETED, responseBody)
 }
 
-func IsStatusRessourceNotFound(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceNotFound(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_NOT_FOUND, responseBody)
 }
 
-func IsStatusRessourceAlreadyExists(responseBody io.ReadCloser) (bool, error) {
+func IsStatusRessourceAlreadyExists(responseBody *io.ReadCloser) (bool, error) {
 	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_ALREADY_EXISTS, responseBody)
 }
--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package apischema
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
@@ -13,13 +14,20 @@ func IsStatus(statusCode int, responseBody string) (bool, error) {
 	return isStatus(statusCode, responseBody)
 }
 
-func IsStatusWithRawBody(statusCode int, responseBody io.ReadCloser) (bool, error) {
-	bytes, err := ioutil.ReadAll(responseBody)
+func IsStatusWithRawBody(statusCode int, responseBody *io.ReadCloser) (bool, error) {
+	byteSlice, err := ioutil.ReadAll(*responseBody)
 	if err != nil {
 		return false, errgo.Mask(err, errgo.Any)
 	}
 
-	return isStatus(statusCode, string(bytes))
+	// This is a hack to be able to read from response body twice. Because we
+	// need to read the response body to identify the actual status of the
+	// response, we consume the stream maybe somebody else would like too. Thus
+	// we buffer the response body and write it back to the original response
+	// body reference.
+	*responseBody = ioutil.NopCloser(bytes.NewReader(byteSlice))
+
+	return isStatus(statusCode, string(byteSlice))
 }
 
 func isStatus(statusCode int, responseBody string) (bool, error) {

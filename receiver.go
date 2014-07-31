4c21cb52f9e3aab124ec6f9d34138c35ca47d69a
Tim Schindler <tim@catalyst-zero.com>
2014-07-31 19:41:56 +0000

    fixed receivers

--- receiver.go
+++ receiver.go
@@ -1,33 +1,37 @@
 package apischema
 
+import (
+  "io"
+)
+
 func IsStatusData(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_DATA, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_DATA, responseBody)
 }
 
 func IsStatusRessourceCreated(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_CREATED, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_CREATED, responseBody)
 }
 
 func IsStatusRessourceStarted(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_STARTED, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_STARTED, responseBody)
 }
 
 func IsStatusRessourceStopped(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_STOPPED, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_STOPPED, responseBody)
 }
 
 func IsStatusRessourceUpdated(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_UPDATED, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_UPDATED, responseBody)
 }
 
 func IsStatusRessourceDeleted(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_DELETED, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_DELETED, responseBody)
 }
 
 func IsStatusRessourceNotFound(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_NOT_FOUND, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_NOT_FOUND, responseBody)
 }
 
 func IsStatusRessourceAlreadyExists(responseBody io.ReadCloser) (bool, error) {
-	return IsStatus(STATUS_CODE_RESSOURCE_ALREADY_EXISTS, responseBody)
+	return IsStatusWithRawBody(STATUS_CODE_RESSOURCE_ALREADY_EXISTS, responseBody)
 }

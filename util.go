5e828f1aa7a37f1c5b7e1346b43f203b4dabc25b
Tim Schindler <tim@catalyst-zero.com>
2014-07-31 19:37:57 +0000

    added method to detect status using raw response body

--- util.go
+++ util.go
@@ -2,12 +2,27 @@ package apischema
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/juju/errgo"
 )
 
 func IsStatus(statusCode int, responseBody string) (bool, error) {
+	return isStatus(statusCode, responseBody)
+}
+
+func IsStatusWithRawBody(statusCode int, responseBody io.ReadCloser) (bool, error) {
+	bytes, err := ioutil.ReadAll(responseBody)
+	if err != nil {
+		return false, errgo.Mask(err, errgo.Any)
+	}
+
+	return isStatus(statusCode, string(bytes))
+}
+
+func isStatus(statusCode int, responseBody string) (bool, error) {
 	var responsePayload ResponsePayload
 	if err := json.Unmarshal([]byte(responseBody), &responsePayload); err != nil {
 		return false, errgo.Mask(err)

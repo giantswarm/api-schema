26ebc803bc7eab1019a769179ab8778d5c106cdf
Stephan Zeissler <stephan@dockzero.io>
2014-11-25 09:51:46 +0100

    Add condition false response

--- responder.go
+++ responder.go
@@ -52,6 +52,10 @@ func StatusConditionTrue() *Response {
 	return NewEmptyResponse(STATUS_CODE_CONDITION_TRUE, "condition true")
 }
 
+func StatusConditionFalse() *Response {
+	return NewEmptyResponse(STATUS_CODE_CONDITION_TRUE, "condition true")
+}
+
 func StatusWrongInput() *Response {
 	return StatusWrongInputWithReason("")
 }

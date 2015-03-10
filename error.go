340d3f025fd4894367e8fe4aabe6913d3c6d3a2e
Ewout Prangsma <ewout@prangsma.net>
2015-03-10 08:25:50 +0000

    Added IsWrongInputWithReasonError method

--- error.go
+++ error.go
@@ -1,5 +1,9 @@
 package apischema
 
+import (
+	"strings"
+)
+
 type ResponseError struct {
 	statusCode int
 	statusText string
@@ -20,47 +24,63 @@ func (err *ResponseError) StatusCode() int {
 	return err.statusCode
 }
 
-func checkResponseErrorStatusCode(err error, statusCode int) bool {
+func checkResponseErrorStatusCode(err error, statusCode int, reason string) bool {
 	respErr, ok := err.(*ResponseError)
 	if !ok {
 		return false
 	}
 
-	return respErr.StatusCode() == statusCode
+	if respErr.StatusCode() != statusCode {
+		return false
+	}
+
+	if reason != "" {
+		// Check reason part of the status text.
+		// See also util.go#newReason
+		if !strings.HasSuffix(respErr.statusText, ": "+reason) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func IsResourceUpError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_UP)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_UP, "")
 }
 
 func IsResourceDownError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_DOWN)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_DOWN, "")
 }
 
 func IsResourceNotFoundError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_NOT_FOUND)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_NOT_FOUND, "")
 }
 
 func IsResourceAlreadyExistsError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_ALREADY_EXISTS)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_ALREADY_EXISTS, "")
 }
 
 func IsResourceInvalidCredentialsError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_INVALID_CREDENTIALS)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_RESOURCE_INVALID_CREDENTIALS, "")
 }
 
 func IsWrongInputError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_WRONG_INPUT)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_WRONG_INPUT, "")
+}
+
+func IsWrongInputWithReasonError(err error, reason string) bool {
+	return checkResponseErrorStatusCode(err, STATUS_CODE_WRONG_INPUT, reason)
 }
 
 func IsUserError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_USER_ERROR)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_USER_ERROR, "")
 }
 
 func IsServerError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_SERVER_ERROR)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_SERVER_ERROR, "")
 }
 
 func IsInvalidVersionError(err error) bool {
-	return checkResponseErrorStatusCode(err, STATUS_CODE_INVALID_VERSION_ERROR)
+	return checkResponseErrorStatusCode(err, STATUS_CODE_INVALID_VERSION_ERROR, "")
 }
